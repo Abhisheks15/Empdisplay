9e11d3b97c6c036207cd7d9c9cae42b0631b2a61
Abhisheks15 <abhishek6307100065@gmail.com>
2020-12-03 15:50:03 +0530

    create model

---
+++ Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppGTExer01.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+
+    }
+}
